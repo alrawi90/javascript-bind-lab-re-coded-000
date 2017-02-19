8bb8e0333af68a23f763e228ec4b7b29d7f2e2db
ALI ALRAWI <ali.developer90@gmail.com>
2017-02-19 07:34:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much.";
+const digitalClock={
+  time: Math.round(Date.now() / 1000),
+  startTicking:function(){
+    //setTimeout(this,1000)
+      setInterval(() => this.time++, 1000);
+
+  }
+}
+function censor(sub,str){
+  return str.replace(new RegExp(sub, 'g'), "BLEEP");
+
+}
+const violenceCensor=censor.bind(null,"violence")
+const drugsCensor=censor.bind(null,"drugs")
